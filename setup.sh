beefd4ded6cb91b2e1164ee189e73b4ae66880fb
Arthur <arthur@pop-os.localdomain>
2020-03-17 18:34:00 -0300

    Add local bin to path

--- setup.sh
+++ setup.sh
@@ -1,9 +1,34 @@
+#!/bin/sh
+# ___  ___  ___ ______ _____  ________   __
+# |  \/  | / _ \|  _  \  ___| | ___ \ \ / /
+# | .  . |/ /_\ \ | | | |__   | |_/ /\ V /
+# | |\/| ||  _  | | | |  __|  | ___ \ \ /
+# | |  | || | | | |/ /| |___  | |_/ / | |
+# \_|  |_/\_| |_/___/ \____/  \____/  \_/
+# ______ _____ _____ _____  _____
+# | ___ \  ___|_   _/  ___||  _  |
+# | |_/ / |__   | | \ `--. | |/' |
+# |    /|  __|  | |  `--. \|  /| |
+# | |\ \| |___ _| |_/\__/ /\ |_/ /
+# \_| \_\____/ \___/\____/  \___/
+
 # APENAS RODAR, SO PRECISA TER O PIP FUNCIONANDO
 
+# checa se o comando pra o virtualenv existe
 if command -v virtualenv; then
 	continue
 else
-	pip install virtualenv
+	# instala o virtualenv e adiciona ao path (NAO FUNCIONA COM FISH)
+	pip install virtualenv 
+	if test -e $HOME/.zshrc; then
+		echo export PATH=\"$HOME/.local/bin:$PATH\" >> $HOME/.zshrc
+		source $HOME/.zshrc
+	elif test -e $HOME/.bashrc; then
+		echo export PATH=\"$HOME/.local/bin:$PATH\" >> $HOME/.bashrc
+		source $HOME/.bashrc
+	else
+		echo "ERROR: nao pode colocar o path no arquivo"
+	fi
 fi
 
 virtualenv -p python3 venv
